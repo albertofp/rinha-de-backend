890745c899efef1ac22d665a7f9365c85a8f2155
Alberto Pluecker <albertopluecker@gmail.com>
2023-08-25 20:43:17 +0200

    add check to apelido on post request

--- handlers/create_person.go
+++ handlers/create_person.go
@@ -6,6 +6,7 @@ import (
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
+	"go.mongodb.org/mongo-driver/bson"
 
 	"github.com/albertofp/rinha-de-backend/database"
 	"github.com/albertofp/rinha-de-backend/models"
@@ -27,12 +28,18 @@ func Create(c *fiber.Ctx) error {
 	//TODO: check if nickname already in db -> skip
 	newPerson := new(models.PersonDTO)
 	if err := c.BodyParser(newPerson); err != nil {
-		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{})
+		return c.Status(fiber.StatusUnprocessableEntity).JSON(fiber.Map{"error": err.Error()})
 	}
 
 	statusCode, res := validation.ValidateRequest(newPerson)
 	if statusCode != fiber.StatusCreated {
 		return c.Status(statusCode).JSON(fiber.Map{"error": res})
+	}
+
+	if personExists, err := checkPersonExists(newPerson.Apelido); err != nil {
+		return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{"error": err.Error()})
+	} else if personExists {
+		return c.Status(fiber.StatusConflict).JSON(fiber.Map{"error": "Person with the same 'apelido' already exists"})
 	}
 
 	newPerson.Id = uuid.New().String()
@@ -46,4 +53,16 @@ func Create(c *fiber.Ctx) error {
 
 	c.Set("Location", fmt.Sprintf("/pessoas/%s", newPerson.Id))
 	return c.Status(statusCode).JSON(fiber.Map{"success": res, "id": newPerson.Id})
+}
+
+func checkPersonExists(apelido string) (bool, error) {
+	coll := database.GetCollection("pessoas")
+
+	filter := bson.M{"apelido": apelido}
+	count, err := coll.CountDocuments(context.TODO(), filter)
+	if err != nil {
+		return false, err
+	}
+
+	return count > 0, nil
 }
